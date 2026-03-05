a09ed92d574fcf465a5d9c4ce077fdfb7f102e60
Shai Petel <shai@petel.com>
2026-03-05 09:25:07 -0500

    fixed getStackCallers

--- src/helpers/console.ts
+++ src/helpers/console.ts
@@ -1,16 +1,20 @@
 import { firstOrNull } from "./collections.base";
 import { isNotEmptyArray } from "./typecheckers";
 
-/** looks through the current call stack to find all calling functions */
+export const getStackCallersDefaultFilter = () => ['node_modules', 'node:internal'];
+/** looks through the current call stack to find all calling functions. default filter: ['node_modules','node:internal'] */
 export function getStackCallers(levels?: number, skip?: number, filter?: string[]) {
-    filter = isNotEmptyArray(filter) ? filter : ['node_modules','node:internal'];
+    filter = isNotEmptyArray(filter) ? filter : getStackCallersDefaultFilter();
     const allLines = new Error().stack.split('\n');
     const stack = allLines
-        .filter(s => s.indexOf('at ') > 0 && !firstOrNull(filter, f => s.includes(f)))
+        .filter(s => s.indexOf('at ') > 0)
         .map(s => {
             const value = s.slice(s.indexOf(' at ') + 4).split(' ');
             return value[0] === "async" ? value[1] : value[0];
-        }).reverse();
+        })
+        //filter based on the names...
+        .filter(s => !firstOrNull(filter, f => s.includes(f)))
+        .reverse();
 
     const end = (skip > 0 ? skip + 1 : 1) * -1;
     const start = levels > 0 ? stack.length - levels + end : 0;
