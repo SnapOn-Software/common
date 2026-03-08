ac77326f73ed3e199b938649ccda4fe2f29f815f
Kevin Vieira <kevin@snapon-software.com>
2026-03-08 10:32:44 -0400

    refactor: remove unused configInfo import and simplify kwiz_cdn_root function

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,4 +1,3 @@
-import { configInfo } from "../_dependencies";
 
 /** apps.kwizcom.com */
 export const kwiz_cdn_hostname_production = "apps.kwizcom.com";
@@ -11,9 +10,11 @@ export const kwiz_cdn_hostname_localdev = "apps.kwizcom.com";
 /** https://apps.kwizcom.com (or local dev/fast-ring) - runtime use only, don't use in places where this might get saved in settings */
 export function kwiz_cdn_root() {
     //must wrap in function otherwise this will eval once before configInfo has been updated.
-    return `https://${configInfo.IsLocalDev
-        ? kwiz_cdn_hostname_localdev
-        : configInfo.IsFastRing
-            ? kwiz_cdn_hostname_fastring
-            : kwiz_cdn_hostname_production}`;
+    // return `https://${configInfo.IsLocalDev
+    //     ? kwiz_cdn_hostname_localdev
+    //     : configInfo.IsFastRing
+    //         ? kwiz_cdn_hostname_fastring
+    //         : kwiz_cdn_hostname_production}`;
+
+    return kwiz_cdn_hostname_production;
 }
