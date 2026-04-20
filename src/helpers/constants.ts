00c7cdb0d3991902bf4f72bb0a815f4807630509
Shai Petel <shai@petel.com>
2026-04-20 11:43:14 -0400

    restored kwiz_cdn_root to environment based

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,3 +1,4 @@
+import { CommonConfig } from "../common-config";
 
 /** apps.kwizcom.com */
 export const kwiz_cdn_hostname_production = "apps.kwizcom.com";
@@ -6,15 +7,15 @@ export const kwiz_cdn_hostname_fastring = "kwizappsfr.azurewebsites.net";
 /** localhost:4433 */
 export const kwiz_cdn_hostname_localdev = "apps.kwizcom.com";
 
-
-/** https://apps.kwizcom.com (or local dev/fast-ring) - runtime use only, don't use in places where this might get saved in settings */
+/** https://apps.kwizcom.com (or local dev/fast-ring) - runtime use only, don't use in places where this might get saved in settings
+ * used to load resources directly from the environment with no need for a redirect tool
+ * redirect tool still needed for entry-point package such as SPFx
+ */
 export function kwiz_cdn_root() {
     //must wrap in function otherwise this will eval once before configInfo has been updated.
-    // return `https://${configInfo.IsLocalDev
-    //     ? kwiz_cdn_hostname_localdev
-    //     : configInfo.IsFastRing
-    //         ? kwiz_cdn_hostname_fastring
-    //         : kwiz_cdn_hostname_production}`;
-
-    return `https://${kwiz_cdn_hostname_production}`
+    return `https://${CommonConfig.i.IsLocalDev
+        ? kwiz_cdn_hostname_localdev
+        : CommonConfig.i.IsFastRing
+            ? kwiz_cdn_hostname_fastring
+            : kwiz_cdn_hostname_production}`;
 }
