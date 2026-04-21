ef1e55b6f088000e3a29d6ec7ae38c77ce9a9ef5
Shai Petel <shai@petel.com>
2026-04-21 11:14:56 -0400

    added some postMessage helpers

---
+++ src/helpers/post-message.ts
@@ -0,0 +1,33 @@
+type resize = {
+    type: "SOS_RESIZE_IFRAME";
+    height: number;
+};
+type visible = {
+    type: "SOS_VISIBILITY_CHANGE";
+    isVisible: boolean;
+};
+
+type syncPostMessageValues = resize | visible;
+
+export function postMessageListener<T extends syncPostMessageValues["type"]>(
+    type: T,
+    handler: (data: Extract<syncPostMessageValues, { type: T }>) => void
+) {
+    window.addEventListener('message', (event: MessageEvent<any>) => {
+        if (event.data && event.data.type === type) {
+            handler(event.data as Extract<syncPostMessageValues, { type: T }>);
+        }
+    }, false);
+}
+
+export function postMessageSender(
+    /** window.parent, window.top, or iframe ID */
+    target: Window | HTMLIFrameElement,
+    data: syncPostMessageValues
+) {
+    let targetWindow = target instanceof HTMLIFrameElement
+        ? target.contentWindow
+        : target;
+
+    targetWindow.postMessage(data, '*');
+}
